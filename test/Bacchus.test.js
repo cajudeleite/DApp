9d02432e1a533adcc51d0cf21b862be31134af68
Caju <nathandebaz266@gmail.com>
2022-10-12 13:44:11 +0200

    all testing is good

--- test/Bacchus.test.js
+++ test/Bacchus.test.js
@@ -1,16 +1,25 @@
 const { expect } = require("chai");
 
 describe("Bacchus", () => {
-  it("Create event", async () => {
+  it("Testing _createEvent", async () => {
     const BacchusContract = await ethers.getContractFactory("Bacchus");
     const bacchusContract = await BacchusContract.deploy();
 
-    await expect(bacchusContract._createEvent("Test", "This is a test", "At my place", "Tomorrow")).to.emit(bacchusContract, "NewEvent").withArgs();
+    await expect(bacchusContract._createEvent("Test", "This is a test", "At my place", "Tomorrow"))
+      .to.emit(bacchusContract, "NewEvent")
+      .withArgs(0, "Test", "This is a test");
   });
-  // it("Create event", async () => {
-  //   const BacchusContract = await ethers.getContractFactory("Bacchus");
-  //   const bacchusContract = await BacchusContract.deploy();
+  it("Testing _getEvent", async () => {
+    const BacchusContract = await ethers.getContractFactory("Bacchus");
+    const bacchusContract = await BacchusContract.deploy();
 
-  //   await expect(bacchusContract._createEvent("Test", "This is a test", "At my place", "Tomorrow")).to.emit(bacchusContract, "NewEvent");
-  // });
+    await bacchusContract._createEvent("Test", "This is a test", "At my place", "Tomorrow");
+    const response = await bacchusContract._getEvent(0);
+
+    expect(response).not.to.be.undefined;
+    expect(response).to.have.property("name", "Test");
+    expect(response).to.have.property("description", "This is a test");
+    expect(response).to.have.property("location", "At my place");
+    expect(response).to.have.property("date", "Tomorrow");
+  });
 });
